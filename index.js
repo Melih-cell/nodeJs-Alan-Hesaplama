aba02bcb971132a17c95fda5ae556b39d3514569
Melih <81979457+Melih-cell@users.noreply.github.com>
2022-07-04 21:42:24 +0300

    mast

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const arg = process.argv.slice(2)
-
-
-const PI = 3.14;
-
-// pi * r * 2
-
-const fonks= (r) => {
-   const alan =  PI * r * 2;
-    console.log(`yarıçap: ${r}, alan: ${alan}`)
-}
-
-fonks(arg[0])
+yeimnn
